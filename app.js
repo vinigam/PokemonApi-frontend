e94c5be929bcd79e153956bb7b46ba2bdff20157
vinigam <viniciusg07@protonmail.com>
2021-05-23 13:28:37 -0300

    fix way that are received data(some pokemons wasnt sorted), add header and navbar with regions to select pokemons

--- app.js
+++ app.js
@@ -28,98 +28,116 @@ function fetchPokemon(region_name){
         start = 810 ; final = 898;
     }
     
+    clearContent("pokemon-container");
     /*fetch requests in selected region */
-    const pokemons = [];
+    var pokemons = [];
     if(start > 0 && final > 0){
-        var pokemon_container = document.getElementsByClassName('pokemon-container');
+        /*promises */
+       
         for (start ; start <= final ; start++){
-            const fetchPokemon = fetch(url + start + '/')
-                .then((response) =>{
-                    return response.json();
-                })
-                .then(response =>{
-                    /*Creating a pokemon box with info */
-                    pokemons.push(response);
-
-                    /*Box settings*/
-                    var pokemon_block = document.createElement('div')
-                    pokemon_block.classList.add("pokemon-block");
-
-                    /*Id settings */
-                    var pokemon_id = document.createTextNode(response.id);
-                    var pokemon_id_box = document.createElement('div');
-                    pokemon_id_box.classList.add('pokemon-id');
-                    pokemon_id_box.appendChild(pokemon_id);
-                    pokemon_block.appendChild(pokemon_id_box);
-
-                    /*Image settings */
-                    var pokemon_img = document.createElement("img");
-                    var img = response['sprites']['front_default'];
-                    pokemon_img.setAttribute('src', img);
-                    pokemon_block.appendChild(pokemon_img);
-
-                    /*Name settings */
-                    var pokemon_name = document.createTextNode('Name : ' + response.name);
-                    var pokemon_name_box = document.createElement('div');
-                    pokemon_name_box.classList.add('pokemon-name');
-                    pokemon_name_box.appendChild(pokemon_name)
-                    pokemon_block.appendChild(pokemon_name_box);
-                    
-                    /*Type settings */
-                    var tag_box = document.createElement('div');
-                    tag_box.classList.add('tag');
-                    var tag = document.createTextNode('Type(s) :');
-                    tag_box.appendChild(tag);
-                    pokemon_block.appendChild(tag_box);
-                    
-                    var type_container = document.createElement('div');
-                    type_container.classList.add('type-container');
-
-                    if(response.types.length > 1){
-                        
-                        let temp = '';
-                        for(let i = 0 ; i < response.types.length ; i++){
-                            let type = document.createTextNode(response.types[i].type.name);
-                            temp = temp + ' ' + response.types[i].type.name;
-                            var type_box = document.createElement('div');
-                            type_box.classList.add('pokemon-type');
-                            type_box.appendChild(type);
-                            type_container.appendChild(type_box)
-                            pokemon_block.appendChild(type_container);
-                            
-                        }
-
-                    }else{
-                        
-                        var type = document.createTextNode(response.types[0].type.name);
-                        var type_box = document.createElement('div');
-                        type_box.classList.add('pokemon-type');
-                        type_box.appendChild(type);
-                        type_container.appendChild(type_box);
-                        pokemon_block.appendChild(type_container);
-                        
-                    }
-                    colorType();
-                    /*More info settings */
-                    var btn_info = document.createElement('div');
-                    btn_info.classList.add('btn-info');
-                    var btn_text = document.createTextNode('More info');
-                    btn_info.appendChild(btn_text);
-                    pokemon_block.appendChild(btn_info);
-                    /*Adding to html*/
-                    
-                    
-                    
-                    pokemon_container[0].appendChild(pokemon_block);
-                    
-                })
+            pokemons.push(
+                fetch(url + start + '/').
+                then(response => response.json())
+            )
         }
+        
+        /*promises in parallel */
+        Promise.all(pokemons)
+        .then(pokemons =>{
+            
+            buildHtml(pokemons);
+           
+            
+        })
+        
     }
 }
 
+function buildHtml(pokemons){
+  
+    var pokemon_container = document.getElementsByClassName('pokemon-container');
+
+    for(let start = 0; start < pokemons.length ; start++){
+        
+        /*Box settings*/
+        var pokemon_block = document.createElement('div')
+        pokemon_block.classList.add("pokemon-block");
+
+        /*Id settings */
+       
+        var pokemon_id = document.createTextNode(pokemons[start].id);
+        var pokemon_id_box = document.createElement('div');
+        pokemon_id_box.classList.add('pokemon-id');
+        pokemon_id_box.appendChild(pokemon_id);
+        pokemon_block.appendChild(pokemon_id_box);
+
+        /*Image settings */
+        var pokemon_img = document.createElement("img");
+        pokemon_img.classList.add('img-pokemon')
+        var img = pokemons[start]['sprites']['front_default'];
+        pokemon_img.setAttribute('src', img);
+        pokemon_block.appendChild(pokemon_img);
+
+        /*Name settings */
+        var pokemon_name = document.createTextNode('Name : ' + pokemons[start].name);
+        var pokemon_name_box = document.createElement('div');
+        pokemon_name_box.classList.add('pokemon-name');
+        pokemon_name_box.appendChild(pokemon_name)
+        pokemon_block.appendChild(pokemon_name_box);
+        
+        /*Type settings */
+        var tag_box = document.createElement('div');
+        tag_box.classList.add('tag');
+        var tag = document.createTextNode('Type(s) :');
+        tag_box.appendChild(tag);
+        pokemon_block.appendChild(tag_box);
+        
+        var type_container = document.createElement('div');
+        type_container.classList.add('type-container');
+
+        if(pokemons[start].types.length > 1){
+            
+            let temp = '';
+            for(let i = 0 ; i < pokemons[start].types.length ; i++){
+                let type = document.createTextNode(pokemons[start].types[i].type.name);
+                temp = temp + ' ' + pokemons[start].types[i].type.name;
+                var type_box = document.createElement('div');
+                type_box.classList.add('pokemon-type');
+                type_box.appendChild(type);
+                type_container.appendChild(type_box)
+                pokemon_block.appendChild(type_container);
+                
+            }
+
+        }else{
+            
+            var type = document.createTextNode(pokemons[start].types[0].type.name);
+            var type_box = document.createElement('div');
+            type_box.classList.add('pokemon-type');
+            type_box.appendChild(type);
+            type_container.appendChild(type_box);
+            pokemon_block.appendChild(type_container);
+            
+        }
+        
+        /*More info settings */
+        var btn_info = document.createElement('div');
+        btn_info.classList.add('btn-info');
+        var btn_text = document.createTextNode('More info');
+        btn_info.appendChild(btn_text);
+        pokemon_block.appendChild(btn_info);
+        /*Adding to html*/
+        
+        
+        
+        pokemon_container[0].appendChild(pokemon_block);
+    }
+    colorType();
+}
 function colorType(){
     let selectColor = document.getElementsByClassName('pokemon-type');
     for(let i = 0 ; i < selectColor.length; i++){
+        
         if(selectColor[i].innerHTML =="normal"){
             selectColor[i].style.backgroundColor = "#aa9";
         }else
@@ -178,4 +196,8 @@ function colorType(){
     }
 }
 
-fetchPokemon("Hoenn");
+function clearContent(className){
+    let toClear = document.getElementsByClassName(className);
+    toClear[0].innerHTML = "";
+}
+fetchPokemon("Kanto");
